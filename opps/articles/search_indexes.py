d4606cc85575014d4c8936b31cb13917b736c302
Davi Oliveira <arlocm@gmail.com>
2013-08-30 01:22:19 -0300

    adding links to haystack index

--- opps/articles/search_indexes.py
+++ opps/articles/search_indexes.py
@@ -3,7 +3,7 @@
 from haystack.indexes import SearchIndex, Indexable, CharField, DateTimeField
 from haystack.indexes import BooleanField
 
-from .models import Post, Album
+from .models import Post, Album, Link
 
 
 Post.is_legacy = lambda self: False
@@ -33,3 +33,16 @@ class AlbumIndex(SearchIndex, Indexable):
 
     def get_model(self):
         return Album
+
+
+class LinkIndex(SearchIndex, Indexable):
+    text = CharField(document=True, use_template=True)
+    date_available = DateTimeField(model_attr='date_available')
+    date_update = DateTimeField(model_attr='date_update')
+    published = BooleanField(model_attr='published')
+
+    def get_updated_field(self):
+        return 'date_update'
+
+    def get_model(self):
+        return Link
